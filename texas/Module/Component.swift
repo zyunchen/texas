fbbfc5ed7cec57059b3e4e72d98fbc3fb77264d9
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2017-07-05 00:35:12 +1200

    commit for save

---
+++ texas/Module/Component.swift
@@ -0,0 +1,21 @@
+//
+//  Component.swift
+//  texas
+//
+//  Created by Liguo Jiao on 5/07/17.
+//  Copyright © 2017 Yunchen Zhang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+private let _componentInstance = Component()
+
+class Component: NSObject {
+    class var shared: Component {
+        return _componentInstance
+    }
+    
+    
+}
